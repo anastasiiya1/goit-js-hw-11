7fc972329e8e6c7c97f55b11ef2f4d422f7d2f90
anastasiiya1 <149606692+anastasiiya1@users.noreply.github.com>
2024-02-24 12:46:09 +0000

    Deploying to gh-pages from @ anastasiiya1/goit-js-hw-11@43d086dddecf3d68b7784bad0a91e68ccd98b824 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,24 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as i}from"./assets/vendor-870f0eb5.js";const e=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake Sailing"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",description:"Alpine Spring Meadows"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",description:"Nature Landscape"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",description:"Lighthouse Coast Sea"}],n=document.querySelector(".gallery");e.forEach(o=>{const t=document.createElement("li");t.classList.add("gallery-item");const a=document.createElement("a");a.classList.add("gallery-link"),a.href=o.original;const p=document.createElement("img");p.classList.add("gallery-image"),p.src=o.preview,p.alt=o.description,p.width=360,p.height=200,a.appendChild(p),t.appendChild(a),n.appendChild(t)});new i(".gallery a",{captionsData:"alt",captionDelay:250,closeOnOverlayClick:!1});
+import{S as f,i as a}from"./assets/vendor-5b791d57.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const d="42469793-94d748bc29f10cf193212e81f",m="https://pixabay.com/api/?";function h(o){const t=new URLSearchParams({key:d,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${m}${t}`).then(i=>{if(!i.ok)throw new Error("Failed to fetch images. Please try again later.");return i.json()})}const l=document.querySelector(".card-list"),p=document.querySelector(".loader"),g=new f(".card-item a",{captionsData:"alt",captionDelay:250});function y({hits:o}){if(!o.length){a.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});return}const t=L(o);l.insertAdjacentHTML("beforeend",t),g.refresh()}function L(o){return o.map(({webformatURL:t,largeImageURL:i,tags:n,likes:e,views:r,comments:s,downloads:u})=>`<li class="card-item">
+  <a href=${i}
+    ><img src=${t} alt="${n}" height="200"/>
+    <ul class="card-info">
+      <li>
+        Likes
+        <p>${e}</p>
+      </li>
+      <li>
+        Views
+        <p>${r}</p>
+      </li>
+      <li>
+        Comments
+        <p>${s}</p>
+      </li>
+      <li>
+        Downloads
+        <p>${u}</p>
+      </li>
+    </ul></a
+  >
+</li>`).join("")}function c(){p.classList.toggle("is-hidden")}const S=document.querySelector(".search-form");S.addEventListener("submit",b);function b(o){o.preventDefault();const i=document.querySelector(".search-input").value.trim();if(i==="")return a.error({title:"Error",message:"Please enter a keyword before submitting the form."}),!1;l.innerHTML="",c(),h(i).then(n=>{y(n)}).then(()=>c()).catch(n=>{console.error("Error fetching images:",n),a.error({title:"Error",message:"An unexpected error occurred while fetching images. Please try again."})})}console.log();
 //# sourceMappingURL=commonHelpers.js.map
